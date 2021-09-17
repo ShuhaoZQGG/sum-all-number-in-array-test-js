075f2802f353246ab8447fc0608eedb70605cbe9
Shuhao Zhang <shuhao.zhang@mail.mcgill.ca>
2021-09-17 18:38:34 -0400

    complete

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 function sumItems(array) {
   // Sum all the numbers in the array
+  let result = 0;
+  for (const el of array){
+    if (Array.isArray(el) === true){
+      result += sumItems(el);
+    } else {
+      result += el;
+    }
+  }
+  return result;
 }
 
 module.exports = sumItems;
